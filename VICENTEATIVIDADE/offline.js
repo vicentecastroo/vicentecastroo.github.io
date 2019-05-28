4317a51003644499a1c2f85af1cc200b8c7d8e6e
vicentecastroo <50918876+vicentecastroo@users.noreply.github.com>
2019-05-28 11:00:00 -0300

    Add files via upload

---
+++ VICENTEATIVIDADE/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1559051924,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowupbotton-sheet0.png",
+		"images/arrowupbotton-sheet1.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/arrowleftbutton-sheet1.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/arrowrightbutton-sheet1.png",
+		"images/inimigo-sheet0.png",
+		"images/botondstroy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
